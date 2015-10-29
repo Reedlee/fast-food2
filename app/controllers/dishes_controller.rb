c9a801772e587c8034d058ee8c9af365823f44f9
=Reedlee <=write.to.lee.igor@gmail.com>
2015-10-29 17:54:27 +0600

    added index, show in controllers dishes, restaurants. add bootstrap.created views for controllers

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,3 +1,14 @@
 class DishesController < ApplicationController
+  def index
+    @dishes = Dish.all
+  end
 
+  def show
+    @dish = Dish.find(params[id])
+  end
+
+  private
+  def dish_params
+    params.require(:dish).permit(:name,:cost, :receipt)
+  end
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,3 +1,14 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
 
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :image, :image_name, :description)
+  end
 end
