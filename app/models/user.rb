c4c67ead93c393b68be035ebe2012481a6a61ac1
=Reedlee <=write.to.lee.igor@gmail.com>
2015-10-29 17:24:26 +0600

    add validation in model and devise in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name, presence: true, length: {maximum: 50}
+  validates :phone, presence:  true, length: {maximum: 20}
+  validates :address, presence: true, length: {maximum: 255}
+
 end
