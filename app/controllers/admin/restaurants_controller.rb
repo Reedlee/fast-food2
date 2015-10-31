e915d046fc4d7a52880ad148bb4cc0cc39ec5fb8
=Reedlee <=write.to.lee.igor@gmail.com>
2015-10-31 18:38:04 +0600

    added controller admin/restaurants with scripts index,show,new, create, update, destroy

---
+++ app/controllers/admin/restaurants_controller.rb
@@ -0,0 +1,66 @@
+class Admin::RestaurantsController < AdminController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      upload_picture(@restaurant)
+      redirect_to [:admin, @restaurant]
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+
+    if @restaurant.save
+      upload_picture(@restaurant)
+      redirect_to [:admin, @restaurant]
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    Restaurant.destroy(params[:id])
+    redirect_to admin_restaurants_path
+  end
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :image, :image_name, :review)
+  end
+
+
+  def upload_picture(restaurant)
+    uploaded_file = params[:restaurant][:image_name]
+
+    if !uploaded_file.nil?
+      file_name = restaurant.id.to_s + File.extname(uploaded_file.original_filename)
+      new_file_path = Rails.root.join('public', 'restaurants', file_name)
+
+      File.open(new_file_path, 'wb') do |file|
+        file.write uploaded_file.read
+      end
+
+      restaurant.update_attribute(:image_name, file_name)
+    end
+  end
+
+end
