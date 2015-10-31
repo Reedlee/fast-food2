6db4937f8144c75e7277b5d70c5820b7e4fbd063
=Reedlee <=write.to.lee.igor@gmail.com>
2015-10-31 18:34:10 +0600

    added admin for dishes and restaurants in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   root 'restaurants#index'
   resources 'restaurants', 'dishes'
 
+  namespace :admin do
+    resources :dishes, :restaurants
+  end
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
